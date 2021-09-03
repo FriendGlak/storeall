ae003f4ba3e73343d2dd8557a1098d043dec23b4
FriendGlak <camilojheans@gmail.com>
2021-09-03 12:27:16 -0500

    Primer commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const api = "https://fakestoreapi.com/products";
+  const [productos, setProductos] = useState();
+  const fetchApi = async () => {
+    const response = await fetch(api);
+    console.log(response.status);
+    const responseJSON = await response.json();
+    setProductos(responseJSON);
+  };
+
+  const deleteData = async () =>{
+    const response = await fetch(api, {
+        method: 'DELETE', 
+    });
+ 
+   const data = await response.json( );
+    console.log(data);
+ };
+ deleteData();
+
+ 
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Fake Store API</h1>
+        <button>+</button>
+      </div>
+
+      <div className="ContainerStore">
+        {!productos
+          ? "Loading..."
+          : productos.map((producto, index) => {
+              return (
+                <div key={index} className="Card">
+                  <img src={producto.image} />
+                  <h1>{producto.title}</h1>
+                  <button>Eliminar</button>
+                </div>
+              );
+            })}
+      </div>
+    </>
   );
 }
 
